2a5d1bb438a86a6cdceef7a4f255e274de06ca9e
OlegGlo <olegglotov99@gmail.com>
2022-01-22 19:54:11 +0300

    fixes before deadline

--- code.py
+++ code.py
@@ -1,20 +1,23 @@
-studentNum = [123,124,125]
-groups = [[12,13],[124,123,125],[234,543,123],[124,125,127]]
+studentNum = [123,124]
+groups = [[12,13],[123,124],[123,123,124]]
 
 def are_valid_groups(groups, studentNum):
 
     length = len(studentNum)
     count = 0
+    countID = 0
     
     for group in groups:
         count = 0
+        countID = 0
         for students in studentNum:
             if (group.count(students) == 0):
                 break
-            if (group.count(students) == 1):
-                count += 1
+            if (group.count(students) >= 1):
+                count += group.count(students)
+                countID += 1
         
-        if count == length:
+        if (count >= length) and (countID == length):
             return True
         
     return False
