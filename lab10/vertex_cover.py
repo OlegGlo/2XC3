490ef875a8c2db6f952d6fafa50e8a1d02c90970
OlegGlo <olegglotov99@gmail.com>
2022-03-29 00:33:51 -0400

    lab 9 organize and lab 10 init

---
+++ lab10/vertex_cover.py
@@ -0,0 +1,17 @@
+
+from termios import VSTOP
+
+
+def vc_approx1(): #add type
+    return
+
+def vc_approx2(): #add type
+    return
+
+def driver():
+
+    vc_approx1()
+
+    vc_approx2()
+    
+    return
