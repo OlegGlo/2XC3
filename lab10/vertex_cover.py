b3bd48e499d2c9405bef49a563669eacdaba9a7e
OlegGlo <olegglotov99@gmail.com>
2022-03-30 22:00:08 -0400

    work and data so far

--- lab10/vertex_cover.py
+++ lab10/vertex_cover.py
@@ -1,9 +1,61 @@
+import random
 
-from termios import VSTOP
+def edgeListForGraph(g):
+    edges = []
+    for i in range(g.number_of_nodes()):
+        for j in g.adjacent_nodes(i):
+            edges.append((i,j))
+
+    for edge in edges:
+        a = edge[0]
+        b = edge[1]
+        # print(a,b)
+        for check in edges:
+            if (a == check[1] and b == check[0]):
+                edges.remove((check[0],check[1]))
+    return edges
+
+def vc_approx1(g): #add type
+
+    Cov = set()
+    Edges = edgeListForGraph(g)
+
+    while (len(Edges) != 0):
+
+        ranEdge = random.choice(Edges) #can also replace with node 0
+
+        Cov.add(ranEdge[0])
+        Cov.add(ranEdge[1])
 
 
-def vc_approx1(): #add type
-    return
+        if ( len(g.adjacent_nodes(ranEdge[0])) >= len(g.adjacent_nodes(ranEdge[1]))):
+            for i in g.adjacent_nodes(ranEdge[0]):
+                try:
+                    Edges.remove((ranEdge[0],i))
+                    # print("removing edge", ranEdge[0], i)
+                except:
+                    pass
+                try:
+                    Edges.remove((i,ranEdge[0]))
+                    # print("removing edge", i, ranEdge[0])
+                except:
+                    pass
+        else:
+            for j in g.adjacent_nodes(ranEdge[1]):
+                
+                try:
+                    Edges.remove((ranEdge[1],j))
+                    # print("removing edge", ranEdge[1], j)
+                except:
+                    pass
+                try:
+                    Edges.remove((j,ranEdge[1]))        
+                    # print("removing edge", j, ranEdge[1])
+                except:
+                    pass
+
+    vCover = list(Cov)
+    return vCover
 
 def vc_approx2(): #add type
     return
